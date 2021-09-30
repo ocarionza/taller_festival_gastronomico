a99c2a02edc5928d4da4402322fceae97dbd60ab
Jorge I. Meza <jimezam@gmail.com>
2021-09-30 18:09:49 -0500

    Se agregó el middleware de autenticación a las rutas protegidas

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\RestaurantController::class, 'showFrontPage'])->name('front_page.index');
 
 //////////////////////////////////////////////
 
-Route::get('/', [App\Http\Controllers\RestaurantController::class, 'showFrontPage'])->name('front_page.index');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('restaurants', App\Http\Controllers\RestaurantController::class);
+});
 
 // Route::get('/restaurants', [App\Http\Controllers\RestaurantController::class, 'index'])->name('restaurants.index');
 // Route::get('/restaurants/{restaurant}', [App\Http\Controllers\RestaurantController::class, 'show'])->name('restaurants.show');
 // Route::get('/restaurants', [App\Http\Controllers\RestaurantController::class, 'create'])->name('restaurants.create');
-
-Route::resource('restaurants', App\Http\Controllers\RestaurantController::class);
