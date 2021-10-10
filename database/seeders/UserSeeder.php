6f8f23a6aca614e62c0a4516cba2f5acbfada9ff
Santiago-Galvis <santiagogalvisgt@gmail.com>
2021-10-10 15:51:27 -0500

    Se actualiza seeders de usuarios.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,10 +19,37 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name' => 'Jorge I. Meza',
-                'email' => 'jimezam@autonoma.edu.co',
+                'name' => 'Brayan Ocampo',
+                'email' => 'zajoseza@gmail.com',
+                'password' => Hash::make('joseb123'),
+                'type' => 'admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+
+            [
+                'name' => 'Santiago Galvis Tabares',
+                'email' => 'sgalvis@autonoma.edu.co',
                 'password' => Hash::make('hola123'),
                 'type' => 'admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+
+            [
+                'name' => 'Usuario',
+                'email' => 'user@gmail.ocm',
+                'password' => Hash::make('12345678'),
+                'type' => 'user',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+
+            [
+                'name' => 'Owner',
+                'email' => 'owner@gmail.ocm',
+                'password' => Hash::make('12345678'),
+                'type' => 'owner',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
